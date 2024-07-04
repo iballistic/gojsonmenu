ba9ad3c27da5fe1212a896d84217322d99ed9eb8
Telman Rustam <git@rustam.ca>
2024-07-03 23:41:12 -0600

    Adding test files

---
+++ jsonmenu/jsonmenu_test.go
@@ -0,0 +1,39 @@
+package jsonmenu
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+
+	jsonFile, err := os.ReadFile("test.json")
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+
+	var menu JSonMenu
+	err = json.Unmarshal(jsonFile, &menu)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+
+	config_data := menu.ViewGroupBySection("foodmenu")
+	if len(config_data) != 3 {
+		t.Errorf("%s", "found more than 3 sections")
+	}
+	for section, views := range config_data {
+		t.Logf("Section: %s", section.Header)
+		for _, view := range views {
+
+			t.Logf("Cell Key: %s", view.Cell.Key)
+
+		}
+
+	}
+
+}
+
+//https://go.dev/doc/code
+//golang.org/x/example
